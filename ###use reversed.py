67d674679ad5070150280a83d083fb09ece80d04
Gekotown <scobosan@myuax.com>
2024-10-15 09:55:27 +0200

    Add files via upload

---
+++ ###use reversed.py
@@ -0,0 +1,6 @@
+###use reversed
+phrase=input("please write a sentence\n")
+words=phrase.split()
+words.reverse()
+reversed_sentence=' '.join(words)
+print("the reversed word is: ",reversed_sentence)
